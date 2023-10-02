78be7af67b3dc9c4181233366f83d41d0fe8d8a5
amdad-sarkar <88956209+amdad-sarkar@users.noreply.github.com>
2023-10-02 12:33:52 +0000

    variable and data type

---
+++ 01_Basics/variable.js
@@ -0,0 +1,6 @@
+const employeeId = 12345
+let employeeEmail ="add@gmail.com"
+var employeePassword = "12121212"
+employeeCity = "kolkata"
+
+console.table([employeeId,employeeEmail,employeePassword,employeeCity])
