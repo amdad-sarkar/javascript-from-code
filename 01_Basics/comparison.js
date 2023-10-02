dc3b1613a0977d27b3b8e0a478c04561d968140c
amdad-sarkar <88956209+amdad-sarkar@users.noreply.github.com>
2023-10-02 17:40:11 +0000

    comparison

--- 01_Basics/comparison.js
+++ 01_Basics/comparison.js
@@ -8,6 +8,8 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
+
+//we have to ignore tjis type of coparison bcz we have different data type
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
