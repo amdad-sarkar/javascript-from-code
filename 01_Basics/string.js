cd0ddd36d3c09f9590ac5e9806dd8a8ea51dff9a
amdad-sarkar <88956209+amdad-sarkar@users.noreply.github.com>
2023-10-04 05:02:38 +0000

    number and string

--- 01_Basics/string.js
+++ 01_Basics/string.js
@@ -61,6 +61,28 @@ while (!theChar.done && theChar.value !== ' ') {
 
 //String.prototype.at()
 
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+let index = 5;
+
+// console.log(`Using an index of ${index} the character returned is ${sentence.at(index)}`);
+// Expected output: "Using an index of 5 the character returned is u"
+
+index = -4;
+
+// console.log(`Using an index of ${index} the character returned is ${sentence.at(index)}`);
+// Expected output: "Using an index of -4 the character returned is d"
+
+
+
+//charAt()
+
+// const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+// const index = 4;
+
+// console.log(`The character at index ${index} is ${sentence.charAt(index)}`);
+// Expected output: "The character at index 4 is q"
 
 
 
